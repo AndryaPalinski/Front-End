bce68ef31f49f5f9976134adf38f0b3215c73fff
Andrya <130005796+KatteKitty@users.noreply.github.com>
2023-10-18 21:57:50 -0300

    Add files via upload

---
+++ Front-End/Front-End/Front-End (noite)/Trabalho 1-2/script.js
@@ -0,0 +1,15 @@
+const trocarfundo = () =>{
+    
+var button = document.querySelector("body");
+
+button.addEventListener("click", () => {
+
+    button.style.background = "rgb(110, 56, 11)";
+
+    var button2 = document.querySelector("title")
+
+button2.addEventListener("click", () => {
+
+    button2.style.background = "rgba(255, 115, 0, 1)";
+
+})})};
