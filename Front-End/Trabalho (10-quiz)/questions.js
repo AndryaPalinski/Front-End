49a97e239b08d5cbba4d545f5f69dd224aa7468a
Andrya <130005796+KatteKitty@users.noreply.github.com>
2023-10-24 21:52:56 -0300

    Foi :]

---
+++ Front-End/Trabalho (10-quiz)/questions.js
@@ -0,0 +1,16 @@
+let questions = [
+  {
+    question:
+      "Teste 1, 2, 3?",
+    options: ["12", "4", "5", "567"],
+    answer: 1,
+  },
+
+  {
+    question:
+      "Em qual elemento HTML nós colocamos o arquivo com a lógica javascript?",
+    options: ["tag js", "tag script", "tag scripting", "tag javascript"],
+    answer: 1,
+  },
+
+];
